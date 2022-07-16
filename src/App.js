c6c077ef3f4b68844e19e7b39d96595f58def17e
James Riggleman <james.riggleman@yahoo.com>
2022-07-16 16:28:25 +0800

    finished CRUD with react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import { useLazyQuery, useMutation } from '@apollo/client';
 import './App.css';
+import {gql} from '@apollo/client'
+import { useState } from 'react';
+
+
+const CITIESQUERY =gql`query Cities {
+  cities {
+    id
+    city
+    country
+    state
+  }
+}`
+
+const CREATECITYMUTATION=gql`
+mutation AddCity($city: String!, $country: String!, $state: String) {
+  addCity(city: $city, country: $country, state: $state) {
+    id
+    city
+    country
+    state
+  }
+}`
+
+const UPDATECITYMUTATION=gql`
+mutation UpdateCity($id: Int!, $city: String!, $country: String!, $state: String) {
+  updateCity(id: $id, city: $city, country: $country, state: $state) {
+    id
+    city
+    country
+    state
+  }
+}`
+
+  const DELETECITYMUTATION=gql`
+  mutation DeleteCity($id: Int!) {
+    deleteCity(id: $id)
+  }
+  `
 
 function App() {
+  const [id, setId] = useState()
+  const [city, setCity] = useState()
+  const [country, setCountry] = useState()
+  const [state, setState] = useState('')
+
+  const [getCities, {loading, error, data}] = useLazyQuery(CITIESQUERY, {onCompleted: (responseData) => {
+    console.log(responseData)
+  }})
+
+  const[createCity,{loading2, error2, data2}] = useMutation(CREATECITYMUTATION, {onCompleted: (responseData) => {
+    console.log(responseData)
+  }})
+
+  const[updateCity,{loading3, error3, data3}] = useMutation(UPDATECITYMUTATION, {onCompleted: (responseData) => {
+    console.log(responseData)
+  }})
+
+  const[deleteCity,{loading4, error4, data4}] = useMutation(DELETECITYMUTATION, {onCompleted: (responseData) => {
+    console.log(responseData)
+  }})
+
+
+  const handleStateChange = (event) => {
+    setState(event.target.value)
+  }
+
+  const handleIdChange = (event) => {
+    setId(event.target.value)
+  }
+
+  const handleCityChange = (event) => {
+    setCity(event.target.value)
+  }
+
+  const handleCountryChange = (event) => {
+    setCountry(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getCities}>click me</button>
+      <div>
+        <h1>id</h1>
+        <input type={id} value={id} onChange={handleIdChange}/>
+        <h1>city</h1>
+        <input type="text" value={city} onChange={handleCityChange} />
+        <h1>country</h1>
+        <input type="text" value={country}  onChange={handleCountryChange} />
+        <h1>state</h1>
+        <input type="text" value={state} onChange={handleStateChange} />
+        <button onClick={() => createCity({variables: {city: city, state: state, country: country}})}>add city</button>
+
+        <button onClick={() => updateCity({variables: {id: parseInt(id) , city: city, state: state, country: country}})}>update city</button>
+
+        <button onClick={() => deleteCity({variables: {id: parseInt(id)}})}>delete city</button>
+        </div>
+      {/* <button onclick={addCity}>add city</button>
+      <button onclick={updateCity}>update city</button>
+      <button onclick={deleteCity}></button> */}
     </div>
   );
 }
